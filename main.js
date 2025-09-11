fde2e5b4e3bb68521e1ca5ee6ee456ec57adae48
Briella Thompson <briellathompson@Kaylas-MacBook-Air-2.local>
2025-09-11 10:02:06 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+function alertMessage() {
+    alert("message");
+}
+
+alertMessage()
+
+function getWelcomeMessage(){
+    return "Welcome to the Webpage!"
+}
+
+console.log(getWelcomeMessage())
+
+function divide(a, b){
+    return a / b
+}
+
+console.log(divide(10,2))
+
+function multiply(a, b){
+    return a * b
+}
+
+console.log(multiply(10,3))
+
+//it is helpful to use functions instead of repeating code because it is faster and it makes it so when you need to reuse that code, you only need to type a little bit for your code to be understood. 
