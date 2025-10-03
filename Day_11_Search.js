716778102fa84227a55a0857f0cb0e2fa2f15cd8
Briella-T <briellat029@gmail.com>
2025-10-03 09:55:03 -0600

    linear, binary, jump searches

--- Day_11_Search.js
+++ Day_11_Search.js
@@ -41,3 +41,22 @@ function binarySearchSorted(sortedArray, target) {
     }
     return -1;
 }
+
+function jumpSearch(arr, target) {
+    const n = arr.length;
+    const step = Math.floor(Math.sqrt(n));
+    let prev = 0;
+    let curr = step;
+
+    while (curr < n && arr[curr] < target) {
+        prev = curr;
+        curr += step;
+    }
+    
+    for (let i = prev; i < Math.min(curr, n); i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
